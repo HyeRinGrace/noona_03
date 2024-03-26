a1173e634739fa0d9ea19d17405414defefbf720
Hyerin Kim <kdjsanh0217@naver.com>
2024-03-26 20:11:51 +0900

    json-server가 최신버전이라 검색기능 구현이 안됐었음...

--- src/Redux/thunk/ProductDetailThunk.js
+++ src/Redux/thunk/ProductDetailThunk.js
@@ -1,6 +1,6 @@
 function getID(id){
     return async(dispatch,getState)=>{
-        let url = `http://localhost:5004/products/${id}`;
+        let url = `https://my-json-server.typicode.com/HyeRinGrace/noona_03/products/${id}`;
         let response = await fetch(url);
         let data = await response.json();
         dispatch({type:"GET_PRODUCT_DETAIL", payload:{data}})
--- src/Redux/thunk/ProductThunk.js
+++ src/Redux/thunk/ProductThunk.js
@@ -1,7 +1,7 @@
 function getProducts(searchQuery) {
     // The `extraArgument` is the third arg for thunk functions
     return async(dispatch, getState) => {
-        let url = `http://localhost:5004/products?q=${searchQuery}`;
+        let url = `https://my-json-server.typicode.com/HyeRinGrace/noona_03/products?q=${searchQuery}`;
         let response = await fetch(url);
         let data = await response.json();
         dispatch({type:"PRODUCT_API",payload:{data}})
